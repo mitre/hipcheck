09a505a264049007c0d34afde7794347fa2997fa
Venus Xeon-Blonde <venus@mitre.org>
2024-07-08 11:50:58 -0400

    fix: Set process-wide default crypto provider from `ring` crate as required by `rustls`.

--- hipcheck/src/http/tls.rs
+++ hipcheck/src/http/tls.rs
@@ -3,7 +3,7 @@
 //! Constructs an [`Agent`] with TLS using system certificates that can be used for making HTTP requests.
 
 use crate::error::Result;
-use rustls::{ClientConfig, RootCertStore};
+use rustls::{crypto::{ring::default_provider, CryptoProvider}, ClientConfig, RootCertStore};
 use std::sync::Arc;
 use ureq::{Agent, AgentBuilder};
 
@@ -13,6 +13,13 @@ pub fn new_agent() -> Result<Agent> {
 	let mut roots = RootCertStore::empty();
 	for cert in rustls_native_certs::load_native_certs()? {
 		roots.add(cert)?;
+	}
+
+	// Rustls makes us setup/install a crypto provider for this process before we can build a client config. 
+	// Use the default provider backed by the `ring` crate. 
+	if CryptoProvider::get_default().is_none() {
+		CryptoProvider::install_default(default_provider())
+			.expect("could not install default crypto provider");
 	}
 
 	// Add certs to connection configuration
