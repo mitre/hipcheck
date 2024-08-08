edab6262454929709fb15a00e80bdfc7f029ae81
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2024-08-08 14:23:37 -0700

    chore: Move 'http' module under 'util' module
    This commit moves the 'http' module under the 'util' module, and updates
    callers.
    
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- hipcheck/src/data/github/mod.rs
+++ hipcheck/src/data/github/mod.rs
@@ -12,7 +12,7 @@ use crate::data::github::data::*;
 use crate::data::github::graphql::get_all_reviews;
 use crate::data::github::graphql_pr::get_all_pr_reviews;
 use crate::error::Result;
-use crate::http::authenticated_agent::AuthenticatedAgent;
+use crate::util::http::authenticated_agent::AuthenticatedAgent;
 use std::sync::Arc;
 
 pub struct GitHub<'a> {
