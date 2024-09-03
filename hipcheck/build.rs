353dbaed5a4508f3ec677f324833a077eb5c28ae
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2024-09-03 15:04:24 -0700

    fix: Broken release build for Hipcheck.
    Turns out Cargo doesn't like when materials required to build a crate are
    outside of the crate manifest directory. In this case, I'm talking about
    the protobuf definition used for the Hipcheck plugin gRPC service.
    
    There's a Cargo issue open about it, but the gist of it is that they
    don't really want people doing what we were trying to do, having materials
    in an outer directory.
    
    There may be better solutions, but moving the files is the fastest one
    for now.
    
    Here's the issue: https://github.com/rust-lang/cargo/issues/3946
    
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- hipcheck/build.rs
+++ hipcheck/build.rs
@@ -1,6 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
-fn main() -> anyhow::Result<()> {
-	tonic_build::compile_protos("../proto/hipcheck/v1/hipcheck.proto")?;
+use anyhow::Result;
+use pathbuf::pathbuf;
+use tonic_build::compile_protos;
+
+fn main() -> Result<()> {
+	let root = env!("CARGO_MANIFEST_DIR");
+	let path = pathbuf![root, "proto", "hipcheck", "v1", "hipcheck.proto"];
+	compile_protos(path)?;
 	Ok(())
 }
