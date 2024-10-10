bf19c91608a23b7481d93271ef3ac0de8af1cd26
Michael Chernicoff <mchernicoff@mitre.org>
2024-10-10 11:01:59 -0400

    feat(refactor): Created plugin version of git queries

---
+++ plugins/git/src/data.rs
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: Apache-2.0
+
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::{
+	fmt::{self, Display, Formatter},
+	hash::Hash,
+	sync::Arc,
+};
+
+/// Commits as they come directly out of `git log`.
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq, Eq)]
+pub struct RawCommit {
+	pub hash: String,
+
+	pub author: Contributor,
+	pub written_on: Result<String, String>,
+
+	pub committer: Contributor,
+	pub committed_on: Result<String, String>,
+}
+
+/// Commits as understood in Hipcheck's data model.
+/// The `written_on` and `committed_on` datetime fields contain Strings that are created from `jiff:Timestamps`.
+/// Because `Timestamp` does not `impl JsonSchema`, we display the datetimes as Strings for passing out of this plugin.
+/// Other plugins that expect a `Timestamp`` should parse the provided Strings into `Timestamps` as needed.
+#[derive(Debug, Serialize, Clone, PartialEq, Eq, Hash, JsonSchema)]
+pub struct Commit {
+	pub hash: String,
+
+	pub written_on: Result<String, String>,
+
+	pub committed_on: Result<String, String>,
+}
+
+impl Display for Commit {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		write!(f, "{}", self.hash)
+	}
+}
+
+/// Authors or committers of a commit.
+#[derive(
+	Debug, PartialEq, Eq, Serialize, Deserialize, Clone, Hash, PartialOrd, Ord, JsonSchema,
+)]
+pub struct Contributor {
+	pub name: String,
+	pub email: String,
+}
+
+impl Display for Contributor {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		write!(f, "{} <{}>", self.name, self.email)
+	}
+}
+
+/// "Join struct" for commits and contributors.
+#[derive(Debug, PartialEq, Eq, Serialize, Clone)]
+pub struct CommitContributor {
+	// Index of commit cache
+	pub commit_id: usize,
+	// Indices of contributor cache
+	pub author_id: usize,
+	pub committer_id: usize,
+}
+
+/// Temporary data structure for looking up the contributors of a commit
+#[derive(Debug, Serialize, Clone, PartialEq, Eq, JsonSchema)]
+pub struct CommitContributorView {
+	pub commit: Commit,
+	pub author: Contributor,
+	pub committer: Contributor,
+}
+
+/// Temporary data structure for looking up the commits associated with a contributor
+#[derive(Debug, Serialize, Clone, PartialEq, Eq, JsonSchema)]
+pub struct ContributorView {
+	pub contributor: Contributor,
+	pub commits: Vec<Commit>,
+}
+
+/// View into commits and diffs joined together.
+#[derive(Debug, Serialize, PartialEq, Eq, JsonSchema)]
+pub struct CommitDiff {
+	pub commit: Commit,
+	pub diff: Diff,
+}
+
+impl Display for CommitDiff {
+	fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+		write!(
+			f,
+			"{} +{} -{}",
+			self.commit,
+			self.diff
+				.additions
+				.map(|n| n.to_string())
+				.as_deref()
+				.unwrap_or("<unknown>"),
+			self.diff
+				.deletions
+				.map(|n| n.to_string())
+				.as_deref()
+				.unwrap_or("<unknown>")
+		)
+	}
+}
+
+/// A set of changes in a commit.
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, JsonSchema)]
+pub struct Diff {
+	pub additions: Option<i64>,
+	pub deletions: Option<i64>,
+	pub file_diffs: Vec<FileDiff>,
+}
+
+/// A set of changes to a specific file in a commit.
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, JsonSchema)]
+pub struct FileDiff {
+	pub file_name: Arc<String>,
+	pub additions: Option<i64>,
+	pub deletions: Option<i64>,
+	pub patch: String,
+}
---
+++ plugins/git/src/main.rs
@@ -0,0 +1,350 @@
+// SPDX-License-Identifier: Apache-2.0
+mod data;
+mod parse;
+mod util;
+
+use crate::{
+	data::{
+		Commit, CommitContributor, CommitContributorView, CommitDiff, Contributor, ContributorView,
+		Diff,
+	},
+	util::git_command::{get_commits, get_commits_from_date, get_diffs},
+};
+use clap::Parser;
+use hipcheck_sdk::prelude::*;
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+
+/// A locally stored git repo, with optional additional details
+/// The details will vary based on the query (e.g. a date, a committer e-mail address, a commit hash)
+#[derive(Clone, Debug, PartialEq, Eq, Serialize, Deserialize, JsonSchema)]
+pub struct GitRepo {
+	/// The path to the repo.
+	pub path: PathBuf,
+
+	/// The Git ref we're referring to.
+	pub git_ref: String,
+
+	/// Optional additional information for the query
+	pub details: Option<String>,
+}
+
+/// Returns the date of the most recent commit to a Git repo as `jiff:Timestamp` displayed as a String
+/// (Which means that anything expecting a `Timestamp` must parse the output of this query appropriately)
+#[query]
+async fn last_commit_date(_engine: &mut PluginEngine, repo: GitRepo) -> Result<String> {
+	let path = &repo.path;
+	let commits = get_commits(path).map_err(|e| {
+		log::error!("failed to get raw commits: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+
+	let first = commits.first().ok_or_else(|| {
+		log::error!("no commits");
+		Error::UnspecifiedQueryState
+	})?;
+
+	first.written_on.clone().map_err(|e| {
+		log::error!("{}", e);
+		Error::UnspecifiedQueryState
+	})
+}
+
+/// Returns all diffs extracted from the repository
+#[query]
+async fn diffs(_engine: &mut PluginEngine, repo: GitRepo) -> Result<Vec<Diff>> {
+	let path = &repo.path;
+	let diffs = get_diffs(path).map_err(|e| {
+		log::error!("{}", e);
+		Error::UnspecifiedQueryState
+	})?;
+	Ok(diffs)
+}
+
+/// Returns all commits extracted from the repository
+#[query]
+async fn commits(_engine: &mut PluginEngine, repo: GitRepo) -> Result<Vec<Commit>> {
+	let path = &repo.path;
+	let raw_commits = get_commits(path).map_err(|e| {
+		log::error!("failed to get raw commits: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+	let commits = raw_commits
+		.iter()
+		.map(|raw| Commit {
+			hash: raw.hash.to_owned(),
+			written_on: raw.written_on.to_owned(),
+			committed_on: raw.committed_on.to_owned(),
+		})
+		.collect();
+
+	Ok(commits)
+}
+
+/// Returns all commits extracted from the repository for a date given in the `details` field
+/// The provided date must be of the form "YYYY-MM-DD"
+#[query]
+async fn commits_from_date(_engine: &mut PluginEngine, repo: GitRepo) -> Result<Vec<Commit>> {
+	let path = &repo.path;
+	let date = match repo.details {
+		Some(date) => date,
+		None => {
+			log::error!("No date provided");
+			return Err(Error::UnspecifiedQueryState);
+		}
+	};
+	// The called function will return an error if the date is not formatted correctly, so we do not need to check for ahead of time
+	let raw_commits_from_date = get_commits_from_date(path, &date).map_err(|e| {
+		log::error!("failed to get raw commits from date: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+	let commits = raw_commits_from_date
+		.iter()
+		.map(|raw| Commit {
+			hash: raw.hash.to_owned(),
+			written_on: raw.written_on.to_owned(),
+			committed_on: raw.committed_on.to_owned(),
+		})
+		.collect();
+
+	Ok(commits)
+}
+
+/// Returns all contributors to the repository
+#[query]
+async fn contributors(_engine: &mut PluginEngine, repo: GitRepo) -> Result<Vec<Contributor>> {
+	let path = &repo.path;
+	let raw_commits = get_commits(path).map_err(|e| {
+		log::error!("failed to get raw commits: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+
+	let mut contributors: Vec<_> = raw_commits
+		.iter()
+		.flat_map(|raw| [raw.author.to_owned(), raw.committer.to_owned()])
+		.collect();
+
+	contributors.sort();
+	contributors.dedup();
+
+	Ok(contributors)
+}
+
+/// Returns all commit-diff pairs
+#[query]
+async fn commit_diffs(engine: &mut PluginEngine, repo: GitRepo) -> Result<Vec<CommitDiff>> {
+	let commits = commits(engine, repo.clone()).await.map_err(|e| {
+		log::error!("failed to get commits: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+	let diffs = diffs(engine, repo).await.map_err(|e| {
+		log::error!("failed to get diffs: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+
+	let commit_diffs = Iterator::zip(commits.iter(), diffs.iter())
+		.map(|(commit, diff)| CommitDiff {
+			commit: commit.clone(),
+			diff: diff.clone(),
+		})
+		.collect();
+
+	Ok(commit_diffs)
+}
+
+/// Returns the commits associated with a given contributor (identified by e-mail address in the `details` value)
+#[query]
+async fn commits_for_contributor(
+	engine: &mut PluginEngine,
+	repo: GitRepo,
+) -> Result<ContributorView> {
+	let email = match repo.details {
+		Some(ref email) => email.clone(),
+		None => {
+			log::error!("No contributor e-maill address provided");
+			return Err(Error::UnspecifiedQueryState);
+		}
+	};
+
+	let all_commits = commits(engine, repo.clone()).await.map_err(|e| {
+		log::error!("failed to get commits: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+	let contributors = contributors(engine, repo.clone()).await.map_err(|e| {
+		log::error!("failed to get contributors: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+	let commit_contributors = commit_contributors(engine, repo.clone())
+		.await
+		.map_err(|e| {
+			log::error!("failed to get join table: {}", e);
+			Error::UnspecifiedQueryState
+		})?;
+
+	// Get the index of the contributor
+	let contributor_id = contributors
+		.iter()
+		.position(|c| c.email == email)
+		.ok_or_else(|| {
+			log::error!("failed to find contributor");
+			Error::UnspecifiedQueryState
+		})?;
+
+	// Get the contributor
+	let contributor = contributors[contributor_id].clone();
+
+	// Find commits that have that contributor
+	let commits = commit_contributors
+		.iter()
+		.filter_map(|com_con| {
+			if com_con.author_id == contributor_id || com_con.committer_id == contributor_id {
+				// SAFETY: This index is guaranteed to be valid in
+				// `all_commits` because of how it and `commit_contributors`
+				// are constructed from `db.raw_commits()`
+				Some(all_commits[com_con.commit_id].clone())
+			} else {
+				None
+			}
+		})
+		.collect();
+
+	Ok(ContributorView {
+		contributor,
+		commits,
+	})
+}
+
+/// Returns the contributor view for a given commit (idenftied by hash in the `details` field)
+#[query]
+async fn contributors_for_commit(
+	engine: &mut PluginEngine,
+	repo: GitRepo,
+) -> Result<CommitContributorView> {
+	let hash = match repo.details {
+		Some(ref hash) => hash.clone(),
+		None => {
+			log::error!("No commit hash provided");
+			return Err(Error::UnspecifiedQueryState);
+		}
+	};
+
+	let commits = commits(engine, repo.clone()).await.map_err(|e| {
+		log::error!("failed to get commits: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+	let contributors = contributors(engine, repo.clone()).await.map_err(|e| {
+		log::error!("failed to get contributors: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+	let commit_contributors = commit_contributors(engine, repo.clone())
+		.await
+		.map_err(|e| {
+			log::error!("failed to get join table: {}", e);
+			Error::UnspecifiedQueryState
+		})?;
+
+	// Get the index of the commit
+	let commit_id = commits.iter().position(|c| c.hash == hash).ok_or_else(|| {
+		log::error!("failed to find contributor");
+		Error::UnspecifiedQueryState
+	})?;
+
+	// Get the commit
+	let commit = commits[commit_id].clone();
+
+	// Find the author and committer for that commit
+	commit_contributors
+		.iter()
+		.find(|com_con| com_con.commit_id == commit_id)
+		.map(|com_con| {
+			// SAFETY: These indices are guaranteed to be valid in
+			// `contributors` because of how `commit_contributors` is
+			// constructed from it.
+			let author = contributors[com_con.author_id].clone();
+			let committer = contributors[com_con.committer_id].clone();
+
+			CommitContributorView {
+				commit,
+				author,
+				committer,
+			}
+		})
+		.ok_or_else(|| {
+			log::error!("failed to find contributor info");
+			Error::UnspecifiedQueryState
+		})
+}
+
+/// Internal use function that returns a join table of contributors by commit
+async fn commit_contributors(
+	engine: &mut PluginEngine,
+	repo: GitRepo,
+) -> Result<Vec<CommitContributor>> {
+	let path = &repo.path;
+	let contributors = contributors(engine, repo.clone()).await.map_err(|e| {
+		log::error!("failed to get contributors: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+	let raw_commits = get_commits(path).map_err(|e| {
+		log::error!("failed to get raw commits: {}", e);
+		Error::UnspecifiedQueryState
+	})?;
+
+	let commit_contributors = raw_commits
+		.iter()
+		.enumerate()
+		.map(|(commit_id, raw)| {
+			// SAFETY: These `position` calls are guaranteed to return `Some`
+			// given how `contributors` is constructed from `db.raw_commits()`
+			let author_id = contributors.iter().position(|c| c == &raw.author).unwrap();
+			let committer_id = contributors
+				.iter()
+				.position(|c| c == &raw.committer)
+				.unwrap();
+
+			CommitContributor {
+				commit_id,
+				author_id,
+				committer_id,
+			}
+		})
+		.collect();
+
+	Ok(commit_contributors)
+}
+
+#[derive(Clone, Debug)]
+struct GitPlugin;
+
+impl Plugin for GitPlugin {
+	const PUBLISHER: &'static str = "mitre";
+
+	const NAME: &'static str = "git";
+
+	fn set_config(&self, _config: Value) -> std::result::Result<(), ConfigError> {
+		Ok(())
+	}
+
+	fn default_policy_expr(&self) -> hipcheck_sdk::prelude::Result<String> {
+		Ok("".to_owned())
+	}
+
+	fn explain_default_query(&self) -> hipcheck_sdk::prelude::Result<Option<String>> {
+		Ok(None)
+	}
+
+	queries! {}
+}
+
+#[derive(Parser, Debug)]
+struct Args {
+	#[arg(long)]
+	port: u16,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<()> {
+	let args = Args::try_parse().unwrap();
+	PluginServer::register(GitPlugin {}).listen(args.port).await
+}
