25d0a55712771539c123a946c6a559f4ac61456f
Andrew Lilley Brinker <alilleybrinker@gmail.com>
2024-10-15 12:27:42 -0700

    chore: Improved integration test setup
    Signed-off-by: Andrew Lilley Brinker <alilleybrinker@gmail.com>

--- hipcheck/tests/smoke.rs
+++ hipcheck/tests/smoke.rs
@@ -1,31 +1,32 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use anyhow::Result;
+use pathbuf::pathbuf;
+use std::path::PathBuf;
 use xshell::{cmd, Shell};
 
-// Git reference to target.
-const REF: &str = "fddb21f";
-
-// The Hipcheck repository itself.
-const PKG: &str = "pkg:github/mitre/hipcheck";
-
-// The current `hc` binary built from source.
-const HC: &str = env!("CARGO_BIN_EXE_hc");
-
+// This test just wraps a shell script, so that the running of this shell
+// script can be integrated with Rust's testing infrastructure.
+//
+// It's done this way because Rust's integration tests feature will only
+// ensure compilation of any binary targets associated with the current package,
+// not with the broader workspace. In our case, to do the integration test we
+// need to make sure we've compiled `hc` AND all the individual plugins under
+// the `plugins/` directory. So we delegate all of that to a shell script
+// that sits under a `tests/` directory in the top-level of the repository,
+// and then just loop it into Rust's test system here.
 #[test]
-fn self_run() -> Result<()> {
+fn integration_script() -> Result<()> {
 	let sh = Shell::new()?;
-	cmd!(sh, "{HC} setup").run()?;
-	let result = cmd!(sh, "{HC} check -v quiet --ref {REF} {PKG}").read()?;
-	println!("{}", result);
+	let root = workspace_root();
+	let script = pathbuf![&root, "tests", "integration.sh"];
+	cmd!(sh, "{script}").run()?;
 	Ok(())
 }
 
-#[test]
-fn plugin_init() -> Result<()> {
-	let sh = Shell::new()?;
-	cmd!(sh, "{HC} setup").run()?;
-	let result = cmd!(sh, "{HC} plugin").read()?;
-	println!("{}", result);
-	Ok(())
+fn workspace_root() -> PathBuf {
+	let root = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+	root.parent()
+		.expect("manifest dir has parent directory")
+		.to_owned()
 }
