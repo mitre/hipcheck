24d436790cc3b23637e924a29a0c0a0cf9a0940f
BMARR <bmarr@mitre.org>
2024-11-21 22:43:40 -0500

    chore: remove typo, entropy, churn from hipcheck core

--- hipcheck/src/metric/mod.rs
+++ hipcheck/src/metric/mod.rs
@@ -1,24 +1,21 @@
 // SPDX-License-Identifier: Apache-2.0
 
 pub mod binary_detector;
-pub mod churn;
 pub mod commit_trust;
 pub mod contributor_trust;
-pub mod entropy;
 pub mod fuzz;
 pub mod identity;
 pub mod linguist;
 mod math;
-pub mod typo;
 
 use crate::{
 	config::{AttacksConfigQuery, CommitConfigQuery},
 	data::{git::GitProvider, DependenciesProvider, FuzzProvider, PullRequestReviewProvider},
 	error::Result,
 	metric::{
-		binary_detector::BinaryFile, churn::ChurnOutput, commit_trust::CommitTrustOutput,
-		contributor_trust::ContributorTrustOutput, entropy::EntropyOutput, fuzz::FuzzOutput,
-		identity::IdentityOutput, linguist::Linguist, typo::TypoOutput,
+		binary_detector::BinaryFile, commit_trust::CommitTrustOutput,
+		contributor_trust::ContributorTrustOutput, fuzz::FuzzOutput,
+		identity::IdentityOutput, linguist::Linguist,
 	},
 };
 use std::sync::Arc;
@@ -35,10 +32,6 @@ pub trait MetricProvider:
 	+ FuzzProvider
 	+ PullRequestReviewProvider
 {	
-	/// Returns result of churn metric
-	#[salsa::invoke(churn::churn_metric)]
-	fn churn_metric(&self) -> Result<Arc<ChurnOutput>>;
-
 	/// Returns result of contributor trust metric
 	#[salsa::invoke(commit_trust::commit_trust_metric)]
 	fn commit_trust_metric(&self) -> Result<Arc<CommitTrustOutput>>;
@@ -46,10 +39,6 @@ pub trait MetricProvider:
 	/// Returns result of contributor trust metric
 	#[salsa::invoke(contributor_trust::contributor_trust_metric)]
 	fn contributor_trust_metric(&self) -> Result<Arc<ContributorTrustOutput>>;
-
-	/// Returns result of entropy metric
-	#[salsa::invoke(entropy::entropy_metric)]
-	fn entropy_metric(&self) -> Result<Arc<EntropyOutput>>;
 
 	/// Returns result of identity metric
 	#[salsa::invoke(identity::identity_metric)]
@@ -59,7 +48,4 @@ pub trait MetricProvider:
 	#[salsa::invoke(fuzz::fuzz_metric)]
 	fn fuzz_metric(&self) -> Result<Arc<FuzzOutput>>;
 	 
-	/// Returns result of typo metric
-	#[salsa::invoke(typo::typo_metric)]
-	fn typo_metric(&self) -> Result<Arc<TypoOutput>>;
 }
