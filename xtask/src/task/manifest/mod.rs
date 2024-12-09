a02ceea810e71b0b65d826d919281a0ff7abd785
jlanson <jlanson@mitre.org>
2024-12-09 08:18:22 -0500

    feat: manifest xtask updates download manifests in the website source files
    Signed-off-by: jlanson <jlanson@mitre.org>

--- xtask/src/task/manifest/mod.rs
+++ xtask/src/task/manifest/mod.rs
@@ -1,18 +1,41 @@
 // SPDX-License-Identifier: Apache-2.0
 
-pub mod download_manifest;
+mod download_manifest;
 mod kdl;
-pub mod remote;
+mod local;
+mod remote;
 mod util;
 
+use download_manifest::DownloadManifestEntry;
+
 use anyhow::Result;
+use std::collections::HashSet;
 
 pub fn run() -> Result<()> {
 	let api_token = std::env::var("HC_GITHUB_TOKEN")?;
-	let releases = remote::get_hipcheck_releases(&api_token)?;
+	let releases = remote::get_hipcheck_plugin_releases(&api_token)?;
 
-	for (name, release) in releases.0 {
-		println!("{name}: {release:?}");
+	for (name, remote_entries) in releases.0 {
+		// We know all releases off the hipcheck github repo are mitre-published
+		let local_manifest_path = local::get_download_manifest_path("mitre", &name)?;
+		let local_entries = local::try_parse_download_manifest(&local_manifest_path)?;
+
+		let remote_set = HashSet::<DownloadManifestEntry>::from_iter(remote_entries.into_iter());
+		let local_set = HashSet::<DownloadManifestEntry>::from_iter(local_entries.into_iter());
+
+		let diff = remote_set.difference(&local_set);
+		let diff_vec = diff.collect::<Vec<_>>();
+
+		if diff_vec.is_empty() {
+			continue;
+		}
+		log::info!(
+			"Updating download manifest for '{}' with {} new entries",
+			name,
+			diff_vec.len()
+		);
+
+		local::append_entries_to_file(local_manifest_path, diff_vec.into_iter())?;
 	}
 
 	Ok(())
