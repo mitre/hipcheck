b6ac119da28cff1285f9eea9190e13c98d8bb6ad
Andrew Lilley Brinker <abrinker@mitre.org>
2025-03-26 10:48:26 -0700

    chore: Fixup
    Signed-off-by: Andrew Lilley Brinker <abrinker@mitre.org>

--- site/scripts/src/footer/docs.ts
+++ site/scripts/src/footer/docs.ts
@@ -10,13 +10,10 @@ export function setupDocsNav() {
   try {
     $toggles = querySelectorAll(".docs-nav-toggle");
   } catch (_e) {
-    // Swallow these errors.
     return;
   }
-  
 
-  $toggles.forEach(($toggle) => {  //loop though toggle elements
-    // Get the icon inside the toggle.
+  $toggles.forEach(($toggle) => {
     const $toggleIcon = $toggle.querySelector(
       ".toggle-icon use",
     ) as HTMLElement;
@@ -24,60 +21,85 @@ export function setupDocsNav() {
       console.log(`no icon found for toggle: '${$toggle}'`);
       return;
     }
-    //Get ID 
-    const sectionId = $toggle.dataset.target;
-    if (!sectionId) { //check if empty
-      console.log(`No section ID found for toggle: '${$toggle}'`);
-      return;
-    }
-    const section = document.getElementById(sectionId);
-    if (!section) {
-      console.log(`No section found for ID: '${sectionId}'`);
+
+    const section = sectionForToggle($toggle);
+    if (section === null) {
       return;
     }
 
-    //Restore state from local storaage
-    const isHidden = localStorage.getItem(`docs-nav-${sectionId}`) === "true";
-    if (isHidden) { //if hidden hen collapse 
-      section.classList.add("hidden"); //adds to class 
-      updateIcon($toggleIcon, "chevron-right", "chevron-down");  //reflects toggle state
+    if (getSectionHidden(section)) {
+      section.$sectionList.classList.add("hidden");
+      updateIcon($toggleIcon, "chevron-right", "chevron-down");
     } else {
-      section.classList.remove("hidden");
+      section.$sectionList.classList.remove("hidden");
       updateIcon($toggleIcon, "chevron-down", "chevron-right");
     }
 
-    //Toggle event listener for clicks
-    $toggle.addEventListener("click", (e) => { //listens for clicks on the toggle button
-      e.preventDefault(); //cancels event - default action will not occur
+    $toggle.addEventListener("click", (e) => {
+      e.preventDefault();
 
-      // Find the subsection list associated with the current toggle.
-      const $parent = $toggle.parentElement?.parentElement;
-      if ($parent === null || $parent === undefined) return; //if not found exit
-      const $subsection = $parent.querySelector(  //find subsection from parent
-        ".docs-nav-section",
-      ) as HTMLElement;
-      if ($subsection === null) return;
+      section.$sectionList.classList.toggle("hidden");
 
-      // Hide or show it.
-      $subsection.classList.toggle("hidden");
-      //will update based on visibility
-      if (sectionIsHidden($subsection)) {
+      if (sectionIsHidden(section)) {
         updateIcon($toggleIcon, "chevron-right", "chevron-down");
       } else {
         updateIcon($toggleIcon, "chevron-down", "chevron-right");
+      }
 
-      }  
-      //save state to local storage 
-      localStorage.setItem(`docs-nav-${sectionId}`, sectionIsHidden($subsection).toString());
-
-
-      
+      setSectionHidden(section);
     });
   });
 }
 
+type Section = {
+  // The outer section containing the toggle.
+  $section: HTMLElement;
+  // The toggle used to trigger showing or hiding the section.
+  $toggle: HTMLElement;
+  // The actual list being hidden or shown.
+  $sectionList: HTMLElement;
+};
 
-//check if hidden using ture or false
-function sectionIsHidden($subsection: HTMLElement): boolean {
-  return $subsection.classList.contains("hidden");
+function sectionForToggle($toggle: HTMLElement): Section | null {
+  const $section = $toggle.parentElement?.parentElement;
+  if ($section === null || $section === undefined) {
+    console.log(`No section found for toggle '${$toggle}'`);
+    return null;
+  }
+
+  const $sectionList = $section.querySelector(
+    ".docs-nav-section",
+  ) as HTMLElement;
+
+  if ($sectionList === null) {
+    console.log(`No section list found for toggle '${$toggle}'`);
+    return null;
+  }
+
+  return {
+    $section,
+    $toggle,
+    $sectionList,
+  };
+}
+
+function sectionIsHidden(section: Section): boolean {
+  return section.$sectionList.classList.contains("hidden");
+}
+
+function getSectionHidden(section: Section): boolean {
+  const sectionId = getSectionId(section);
+  return localStorage.getItem(`docs-nav-${sectionId}`) === "true";
+}
+
+function setSectionHidden(section: Section) {
+  const sectionId = getSectionId(section);
+  localStorage.setItem(
+    `docs-nav-${sectionId}`,
+    sectionIsHidden(section).toString(),
+  );
+}
+
+function getSectionId(section: Section): string | undefined {
+  return section.$toggle.dataset.section?.replace(/\s/, "-").toLowerCase();
 }
--- site/scripts/src/header/docs.ts
+++ site/scripts/src/header/docs.ts
@@ -1,8 +1,6 @@
-import {setupDocsNav} from "../footer/docs.ts";
-
 /**
  * Initializes the application by setting up the docs navigation.
  */
 export function setupnav() {
-    setupDocsNav();
-  }
+  // TODO: Implement this.
+}
