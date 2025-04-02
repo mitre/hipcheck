549d62ac9d4ff34c7b95e0d56d3e356ef2e24a0c
jlanson <jlanson@mitre.org>
2025-04-02 09:58:27 -0400

    chore: bump python sdk version
    Signed-off-by: jlanson <jlanson@mitre.org>

--- sdk/python/docs/source/conf.py
+++ sdk/python/docs/source/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../../src/hipcheck_sdk"))
 project = "hipcheck-sdk"
 copyright = "2025, Julian Lanson"
 author = "Julian Lanson"
-release = "0.1.0"
+release = "0.2.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
