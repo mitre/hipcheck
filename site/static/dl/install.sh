30ea85c3ea12297838d2a821db9e50ecd3345d08
Andrew Lilley Brinker <abrinker@mitre.org>
2025-05-02 13:34:26 -0700

    chore: Announced Hipcheck 3.14.0
    Signed-off-by: Andrew Lilley Brinker <abrinker@mitre.org>

--- site/static/dl/install.sh
+++ site/static/dl/install.sh
@@ -3,7 +3,7 @@
 # This installer delegates to the "real" installer included with each new
 # release of Hipcheck.
 
-HC_VERSION="3.13.0"
+HC_VERSION="3.14.0"
 REPO="https://github.com/mitre/hipcheck"
 INSTALLER="$REPO/releases/download/hipcheck-v$HC_VERSION/hipcheck-installer.sh"
 
