9ecd6ed505b9dced3b9a96a2275ff8f79246374d
Andrew Lilley Brinker <abrinker@mitre.org>
2025-11-24 11:26:32 -0800

    feat: initial UserOrgs query impl
    Signed-off-by: Andrew Lilley Brinker <abrinker@mitre.org>

--- plugins/github/src/graphql/mod.rs
+++ plugins/github/src/graphql/mod.rs
@@ -2,5 +2,9 @@
 
 //! Logic for interacting with the GitHub GraphQL API.
 
+mod custom_scalars;
 pub mod reviews;
 pub mod user_orgs;
+
+/// The URL of the GitHub GraphQL API.
+pub const GH_API_V4: &str = "https://api.github.com/graphql";
