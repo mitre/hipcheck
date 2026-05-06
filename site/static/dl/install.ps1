925d5a6c2e549b1a92b5fb576443b1377952f874
Andrew Lilley Brinker <abrinker@mitre.org>
2026-05-06 11:08:26 -0700

    chore: Hipcheck 3.15.0 release blog post
    This announces the 3.15.0 release with a new blog post
    detailing all the improvements.
    
    Signed-off-by: Andrew Lilley Brinker <abrinker@mitre.org>

--- site/static/dl/install.ps1
+++ site/static/dl/install.ps1
@@ -2,7 +2,7 @@
 # This installer delegates to the "real" installer included with each new
 # release of Hipcheck.
 
-$hc_version = "3.14.0"
+$hc_version = "3.15.0"
 $installer = "https://github.com/mitre/hipcheck/releases/download/hipcheck-v${hc_version}/hipcheck-installer.ps1"
 
 irm "$installer" | iex
